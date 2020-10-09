5ea25a1bad10cc1915137db5750f7e22d011be74
Cassio Menezes <cassio.menezes@outlook.com.br>
2020-10-09 13:35:03 -0300

    get

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3300
 
+app.get('/', (req, res) => res.send('Hello World from Express!'))
+
 app.listen(port, () => console.log('API running on port ' + port))
