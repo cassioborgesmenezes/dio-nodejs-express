caf5c70251b02dccb5d814565d8e6a2e7ef16ef9
Cassio Menezes <cassio.menezes@outlook.com.br>
2020-10-14 14:42:50 -0300

    delete

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -48,6 +48,13 @@ const userRoute = (app) => {
 
             res.status(200).send('User ok')
         })
+        .delete((req, res) => {
+            const users = getUsers()
+
+            saveUser(users.filter(user => user.id !== req.params.id))
+
+            res.status(200).send('User deleted.')
+        })
 }
 
 module.exports = userRoute
